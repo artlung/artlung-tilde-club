38c6d5f47c314e69f6c0ccb3ae67a87cb6baa626
Joe Crawford <joe@artlung.com>
2024-11-27 16:25:51 -0800

    Some rewriting and restyling

--- public_html/index.php
+++ public_html/index.php
@@ -20,9 +20,9 @@
 www.w3.org</A></I></P>
 
 <HR>
-
+<H2>Updates</H2>
 <DL>
-  <DT>Update November 2024</DT>
+  <DT>November 2024</DT>
   <DD>
 <UL>
 <LI>I am still <A href="http://artlung.com/blog/">blogging</A>.</LI>
@@ -31,26 +31,26 @@ www.w3.org</A></I></P>
 <LI><A href="http://artlung.com/">My website</A> continues to evolve including more stuff pulled into my WordPress instance.</LI>
 <LI>I&#39;m doing videos <A href="https://www.youtube.com/@artlung">on YouTube</A>.</LI>
 <LI>I love doing <A href="https://cssbattle.dev/player/artlung">CSS Battles</A>!</LI>
-<LI>I added support for accepting webmentions via <A href="https://webmention.io/">webmention.io</A> They're displayed below.</LI>
+<LI>I added support for accepting webmentions via <A href="https://webmention.io/">webmention.io</A>.</LI>
 <LI>The code for this Tilde.Club page is now on <A HREF="https://github.com/artlung/artlung-tilde-club/">GitHub</A></LI>
 </UL>
     </DD>
-  <DT>Update July 2024</DT>
+  <DT>July 2024</DT>
   <DD>
 It's Comic-Con month. It ought to be fun! Rock on people!
   </DD>
   
-  <DT>Update June 2024</DT>
+  <DT>June 2024</DT>
   <DD><A href="https://indieweb.org/Front_End_Study_Hall">FrESH</A> is ongoing and fun to host. I've learned a lot.</DD>
   <DD>I will be going to San Diego Comic-Con in July. And I'll be going to <A href="https://artlung.com/blog/tag/xoxofest/">XOXO Festival</A> in Portland in August. That's nifty.</DD>
-  <DT>Update May 2024</DT>
+  <DT>May 2024</DT>
   <DD>I took over maintenance of <A href="https://blogofthe.day">blogofthe.day</A></DD>
   <DD>I added to my <A href="https://github.com/artlung">github profile</A></DD>
   <DD>Since my last update the first and a second <A href="https://indieweb.org/Front_End_Study_Hall">Front End Study Hall</A> were run. They were both great. And another is upcoming! I created a <A href="https://artlung.com/headers/-20240511">new header</A>. The water in the ocean is getting a bit warmer too. Life is pretty good.</DD>
-  <DT>Update April 2024</DT>
+  <DT>April 2024</DT>
   <DD>I have a new gig I'm enjoying. And I'll be doing a learning and teaching <A href="https://artlung.com/blog/2024/04/15/front-end-study-hall/">event I call "Front End Study Hall"</A> this month and I hope it'll be fun and useful.</DD>
   
-  <DT>Update March 2024</DT>
+  <DT>March 2024</DT>
   <DD>Now I have <A href="https://artlung.com/guestbook/">a Guestbook again</A>, and a <A href="https://artlung.com/contact/">Contact form</A> too, <EM>you know, for the attempted spammers</EM>.
   </DD>
   <DD>I had fun updating the <A href="https://artlung.com/visualization/">Blog Visualization</A> on my site. So many years blogging! Who knew Instagram was the majority of what I'm doing for many years?</DD>
@@ -61,13 +61,23 @@ It's Comic-Con month. It ought to be fun! Rock on people!
   <DD>
     I added a "Lincoln Baio." I think that's what people are saying when they say that.
     <DD>I added a tiny style sheet.
-    <DT>Why does this page look so old?
+    </DD>
+    <DT>2014:
+    <DD>It's fun to make little web pages.
+</DL>
+
+<HR>
+
+<H2>About This Site</h2>
+<DL>
+    <DT>Why does this page look old?
   <DD>It's written in HTML 2.0, an older spec. HTML 2.0 was created in 1995 (<A href="https://www.ietf.org/rfc/rfc1866.txt">RFC 1866</A>). It works just fine. The great thing about conforming to an HTML standard is that it's likely to keep working in decades later.
   <DD>HTML 2.0 referred to style sheets being linkable in LINK tags: <I>"<CODE>&lt;LINK&gt;</CODE> hyperlinks may also be processed without explicit user request; for example, style sheet resources may be processed before or during the processing of the document."</I>
   <DD>It would be another year before a browser shipped CSS.
+    <DD>
+        <I>Fun fact:</I> In HTML 2.0, the allowed attributes of the <CODE>&lt;IMG&gt;</CODE> tag are only <CODE>ALIGN</CODE>, <CODE>ALT</CODE>, <CODE>ISMAP</CODE>, and <CODE>SRC</CODE>.
+    </DD>
   <DD>HTML 2 was superseded in 1997 by <A href="https://www.w3.org/TR/2018/SPSD-html32-20180315/">HTML 3.2</A>. Also now superseded.
-  <DT>Update 2014:
-  <DD>It's fun to make little web pages.
 </DD>
 </DL>
 
